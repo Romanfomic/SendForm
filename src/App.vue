a476357521559603c43bbef296303397ae1977c5
Roman Fomichev <romanfomic@gmail.com>
2024-04-02 10:22:15 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
+        <HeaderComponent></HeaderComponent>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import HeaderComponent from "@/components/HeaderComponent.vue";
     export default {
-
+        components: {
+            HeaderComponent
+        }
     }
 </script>
 
 <style>
-
+    body {
+        margin: 0;
+    }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-
+    <div class="header">
+        <div class="nav-bar">
+            <router-link class="nav-bar-item" to="/">Main</router-link>
+            <router-link class="nav-bar-item" to="/weather">Weather</router-link>
+        </div>
+        <hr/>
     </div>
 </template>
 
@@ -12,5 +16,25 @@
 </script>
 
 <style scoped>
+    .nav-bar-item {
+        text-decoration: none;
+        color: black;
+        font-size: 20px;
+        font-weight: bold;
+        margin: 10px;
+        transition: color 0.2s;
+    }
+    .nav-bar-item:hover {
+        color: dodgerblue;
+    }
+    .header {
+        display: flex;
+        align-items: center;
+        border: 1px solid black;
+    }
+    .nav-bar {
+        display: flex;
+        align-items: center;
+    }
 
 </style>
